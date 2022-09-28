d6e7bd13776e51c1741ecb30488b37a90962b0a4
Doug A <douganderson444@gmail.com>
2022-09-27 21:53:35 -0300

    postcss and replace tweaks

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,29 @@
 // import adapter from '@sveltejs/adapter-auto';
 import adapter from 'sveltejs-adapter-ipfs';
-
 import preprocess from 'svelte-preprocess';
+
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess({
-		postcss: true
+		autoPreprocess: {
+			replace: [[/process\.env\.NODE_ENV/g, JSON.stringify(process.env.NODE_ENV)]]
+		},
+		postcss:
+			process.env.NODE_ENV === 'development'
+				? true // use app.css & +layout.svelte, when developing to preview the styles
+				: {
+						// use @import when building packages for distribution
+						configFilePath: path.resolve(__dirname, './postcss.config.js'),
+						prependData: `@import '${path.resolve('./src/app.css')}';`
+				  }
 	}),
 
 	kit: {
