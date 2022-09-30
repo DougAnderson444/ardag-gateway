02690f56407c37541fba0a0c0946fd64c30f3293
Doug A <douganderson444@gmail.com>
2022-09-30 16:59:06 -0300

    gateway mounted

--- src/lib/ArDagGateway.svelte
+++ src/lib/ArDagGateway.svelte
@@ -1,21 +1,25 @@
 <script>
 	// @ts-nocheck
-	import { Gateway } from '@douganderson444/svelte-component-gateway';
+	import { onMount } from 'svelte';
 
 	export let owner;
 	export let tag;
 	export let ardag;
 	export let dag;
 
+	let Gateway;
 	let esModule;
 	let props;
 	let tagNode;
 	let data;
 
-	$: if (ardag && owner && tag) tagSelected();
+	onMount(async () => {
+		({ Gateway } = await import('@douganderson444/svelte-component-gateway'));
+	});
+
+	$: if (ardag && owner && tag && dag) tagSelected();
 
 	async function tagSelected() {
-		console.log('Selecting tag', owner, tag);
 		try {
 			const rootCID = await ardag.load({ dagOwner: owner, dag });
 			tagNode = (await dag.get(rootCID, { path: `${tag}/obj/` })).value;
@@ -50,10 +54,19 @@
 	<slot {tagNode} {data} />
 {/if}
 
-{#if esModule || (props && esModule)}
+{#if Gateway && (esModule || (props && esModule))}
 	<div class="flex-1 flex flex-col">
 		<div class="flex-1 w-full my-0 mx-auto box-border justify-start">
-			<Gateway {esModule} {props} on:change on:change={(e) => (data = e.detail)} />
+			<svelte:component
+				this={Gateway}
+				{esModule}
+				{props}
+				on:change
+				on:change={(e) => (data = e.detail)}
+			/>
 		</div>
 	</div>
 {/if}
+
+<style lang="postcss">
+</style>
